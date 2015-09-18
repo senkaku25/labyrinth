4ee42d6a0d9025a8a68805b6d7d603f61c8dc0c0
Jeffrey Leung <j.leung51@shaw.ca>
2015-09-17 22:01:49 -0700

    Adding testing implementation for class Room.

---
+++ test/test_room.cpp
@@ -0,0 +1,81 @@
+/*
+ *
+ * Author: Jeffrey Leung
+ * Last edited: 2015-09-17
+ *
+ * This C++ file tests the functionality of room.cpp.
+ *
+ */
+
+#include <iostream>
+
+#include "../room/room_properties.hpp"
+#include "../room/room.hpp"
+
+// This function prints the given type of room border.
+std::string RoomBorderPrint( RoomBorder r )
+{
+  if( r == RoomBorder::kWall )
+  {
+//    std::cout << "Wall";
+    return "Wall";
+  }
+  else if( r == RoomBorder::kRoom )
+  {
+//    std::cout << "Room";
+    return "Room";
+  }
+  else
+  {
+//    std::cout << "Exit";
+    return "Exit";
+  }
+}
+
+int main()
+{
+  std::cout << std::endl;
+  std::cout << "TESTING ROOM.CPP IMPLEMENTATION" << std::endl;
+  std::cout << "________________________________________________" << std::endl;
+  std::cout << std::endl;
+  
+  std::cout << "Creating a room with:" << std::endl;
+  std::cout << "  A minotaur" << std::endl;
+  std::cout << "  A bullet" << std::endl;
+  std::cout << "  An exit north of the room" << std::endl;
+  std::cout << "  A wall to the south of the room" << std::endl;
+  std::cout << "  Entrances to other rooms to the east and west of the room "
+            << std::endl;
+  
+  Room rm_1( Inhabitant::kMinotaur,
+             Item::kBullet,
+             Direction::kNorth,
+             false, false, true, false );
+  std::cout << "Completed." << std::endl;
+  std::cout << "________________________________________________" << std::endl;
+  std::cout << std::endl;
+  
+  std::cout << "Testing DirectionCheck():" << std::endl;
+  std::cout << "  To the north of the room is a "
+            << RoomBorderPrint( rm_1.DirectionCheck(Direction::kNorth) )
+            << "." << std::endl;
+  std::cout << "  To the east of the room is a "
+            << RoomBorderPrint( rm_1.DirectionCheck(Direction::kEast) )
+            << "." << std::endl;
+  std::cout << "  To the south of the room is a "
+            << RoomBorderPrint( rm_1.DirectionCheck(Direction::kSouth) )
+            << "." << std::endl;
+  std::cout << "  To the west of the room is a "
+            << RoomBorderPrint( rm_1.DirectionCheck(Direction::kWest) )
+            << "." << std::endl;
+            
+  std::cout << "________________________________________________" << std::endl;
+  std::cout << std::endl;
+  std::cout << "All tests completed." << std::endl;
+  std::cout << std::endl;
+  std::cout << "Press enter to exit.";
+  getchar();
+  std::cout << std::endl;
+
+  return 0;
+}
