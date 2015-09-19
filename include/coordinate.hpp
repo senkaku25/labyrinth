027116929f31ade54d904e307a85b0d30bcc03d4
Jeffrey Leung <j.leung51@shaw.ca>
2015-09-19 15:06:20 -0700

    Adding a Coordinate struct for the Labyrinth.

---
+++ include/coordinate.hpp
@@ -0,0 +1,17 @@
+/*
+ *
+ * Author: Jeffrey Leung
+ * Last edited: 2015-09-19
+ *
+ * This C++ header file contains the implementation of a Coordinate struct
+ * used by the Labyrinth class
+ *
+ */
+ 
+#pragma once
+
+struct Coordinate
+{
+  unsigned int x;
+  unsigned int y;
+};
