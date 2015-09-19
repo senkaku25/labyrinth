7e1857fd27ead7c8277084c3fc9c85cc27bc5acf
Jeffrey Leung <j.leung51@shaw.ca>
2015-09-19 15:41:54 -0700

    Adding (empty) labyrinth testing.

---
+++ test/test_laby.cpp
@@ -0,0 +1,19 @@
+/*
+ *
+ * Author: Jeffrey Leung
+ * Last edited: 2015-09-19
+ *
+ * This C++ file tests the Labyrinth class implementation.
+ *
+ */
+ 
+#include <iostream>
+
+#include "../include/room_properties.hpp"
+#include "../include/room.hpp"
+#include "../include/labyrinth.hpp"
+
+int main()
+{
+  return 0;
+}
