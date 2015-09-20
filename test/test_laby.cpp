b109b4d0c0be42248951322611b777ae15eff2e2
Jeffrey Leung <j.leung51@shaw.ca>
2015-09-19 21:05:27 -0700

    Adding Labyrinth testing implementation.

--- test/test_laby.cpp
+++ test/test_laby.cpp
@@ -7,7 +7,9 @@
  *
  */
  
+#include <exception>
 #include <iostream>
+#include <stdexcept>
 
 #include "../include/room_properties.hpp"
 #include "../include/room.hpp"
@@ -16,5 +18,93 @@
 
 int main()
 {
+  std::cout << std::endl
+            << "TESTING LABYRINTH.CPP IMPLEMENTATION" << std::endl
+            << "________________________________________________" << std::endl
+            << std::endl;
+  
+  
+  
+  std::cout << "Creating a basic, empty Labyrinth with:" << std::endl
+            << "  x size = 3" << std::endl
+            << "  y size = 5" << std::endl;
+  
+  Labyrinth( 3, 5 );
+  std::cout << "Completed." << std::endl;
+  std::cout << std::endl;
+  
+  
+  
+  std::cout << "Type 1 to attempt to create a Labyrinth with x size = 0."
+            << std::endl
+            << "Type 2 to attempt to create a Labyrinth with y size = 0."
+            << std::endl
+            << "Type 3 to attempt to create a Labyrinth with x size = "
+            << "y size = 0."
+            << std::endl
+            << "Type 0 to continue."
+            << std::endl
+            << "> ";
+        
+  char c = ' ';
+  while ( c < '0' || c > '3' )
+  {
+    std::cin >> c;
+    switch( c )
+    {
+      case( '1' ):
+        try
+        {
+          Labyrinth( 0, 10 );
+        }
+        catch( std::exception& e )
+        {
+          std::cout << e.what() << std::endl;
+        }
+        break;
+      
+      case( '2' ):
+        try
+        {
+          Labyrinth( 10, 0 );
+        }
+        catch( std::exception& e )
+        {
+          std::cout << e.what() << std::endl;
+        }
+        break;
+        
+      case( '3' ):
+        try
+        {
+          Labyrinth( 0, 0 );
+        }
+        catch( std::exception& e )
+        {
+          std::cout << e.what() << std::endl;
+        }
+        break;
+        
+      case( '0' ):
+        break;
+        
+      default:
+        std::cout << std::endl
+                  << "Error: That was not a valid input; please try again: ";
+        break;
+    }
+  }
+  std::cout << std::endl;
+  
+              
+            
+  std::cout << "________________________________________________" << std::endl;
+  std::cout << std::endl;
+  std::cout << "All tests completed." << std::endl;
+  std::cout << std::endl;
+  std::cout << "Press enter to exit.";
+  getchar();
+  std::cout << std::endl;
+  
   return 0;
 }
