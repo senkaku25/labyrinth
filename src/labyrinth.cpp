00be42ad658cbd6f39057d08bf8e2379ccb030f9
jleung51 <jleung513@gmail.com>
2015-12-25 23:23:31 -0800

    Atom doing some mojo with spacing.

--- src/labyrinth.cpp
+++ src/labyrinth.cpp
@@ -7,7 +7,7 @@
  * the Room class to create a 2-d mapping for a game.
  *
  */
- 
+
 #include <cmath>
 #include <iostream>
 #include <stdexcept>
@@ -29,7 +29,7 @@ Labyrinth::Labyrinth( unsigned int x_size, unsigned int y_size )
     if( y_size == 0 )
     {
       throw std::invalid_argument( "Error: Labyrinth() was given empty x and "\
-        "y sizes." );  
+        "y sizes." );
     }
     else
     {
@@ -48,7 +48,7 @@ Labyrinth::Labyrinth( unsigned int x_size, unsigned int y_size )
   {
     rooms_[i] = new Room[ x_size ];
   }
-  
+
   x_size_ = x_size;
   y_size_ = y_size;
 }
@@ -78,7 +78,7 @@ void Labyrinth::ConnectRooms( Coordinate rm_1, Coordinate rm_2 )
     throw std::logic_error( "Error: ConnectRooms() was given the same "\
       "coordinate for the two Rooms." );
   }
-  
+
   else if( !WithinBounds(rm_1) || !WithinBounds(rm_2) )
   {
     if( !WithinBounds(rm_1) )
@@ -94,26 +94,26 @@ void Labyrinth::ConnectRooms( Coordinate rm_1, Coordinate rm_2 )
           "an invalid coordinate for rm_1." );
       }
     }
-    
+
     else
     {
       throw std::invalid_argument( "Error: ConnectRooms() was given an "\
         "invalid coordinate for rm_2." );
     }
   }
-  
+
   else if( !IsAdjacent(rm_1, rm_2) )
   {
     throw std::logic_error( "Error: ConnectRooms() was given two coordinates "\
       "which are not adjacent, and therefore cannot be connected." );
   }
-  
+
   unsigned int x_distance = rm_2.x - rm_1.x;
   unsigned int y_distance = rm_2.y - rm_1.y;
-  
+
   Direction break_wall_1;
   Direction break_wall_2;
-  
+
   if( x_distance == 0 )
   {
     if( y_distance > 0 )  // rm_1 is north of rm_2
@@ -127,7 +127,7 @@ void Labyrinth::ConnectRooms( Coordinate rm_1, Coordinate rm_2 )
       break_wall_2 = Direction::kSouth;
     }
   }
-  
+
   else if( y_distance == 0 )
   {
     if( x_distance > 0 )  // rm_1 is west of rm_2
@@ -141,13 +141,13 @@ void Labyrinth::ConnectRooms( Coordinate rm_1, Coordinate rm_2 )
       break_wall_2 = Direction::kEast;
     }
   }
-  
+
   else
   {
     throw std::logic_error( "Error: ConnectRooms() called IsAdjacent(), "\
       "which should have evaluated to false, but did not." );
   }
-  
+
   RoomAt(rm_1).BreakWall(break_wall_1);
   RoomAt(rm_2).BreakWall(break_wall_2);
   return;
@@ -204,20 +204,20 @@ bool Labyrinth::IsAdjacent( Coordinate rm_1, Coordinate rm_2 )
     throw std::invalid_argument( "Error: IsAdjacent() was given an invalid "\
       "coordinate for rm_2." );
   }
-  
+
   if( rm_1 == rm_2 )
   {
     throw std::logic_error( "Error: IsAdjacent() was given the same "\
       "coordinate for the Rooms." );
   }
-  
+
   unsigned int x_distance = abs( rm_1.x - rm_2.x );
   unsigned int y_distance = abs( rm_1.y - rm_2.y );
-  
+
   if( ( x_distance == 0 && y_distance == 1 ) ||
       ( x_distance == 1 && y_distance == 0 ) )
   {
     return true;
   }
-  return false; 
+  return false;
 }
