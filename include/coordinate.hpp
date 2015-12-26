e21f0a8cf2a5d6a4b615687064cb377bbe793475
jleung51 <jleung513@gmail.com>
2015-12-25 16:18:55 -0800

    Adding = overload for Coordinate.

--- include/coordinate.hpp
+++ include/coordinate.hpp
@@ -1,24 +1,32 @@
 /*
  *
  * Author: Jeffrey Leung
- * Last edited: 2015-09-19
+ * Last edited: 2015-12-25
  *
  * This C++ header file contains the implementation of a Coordinate struct
  * used by the Labyrinth class.
  *
  */
- 
+
 #pragma once
 
 struct Coordinate
 {
   unsigned int x;
   unsigned int y;
-  
+
   // Operator overload for ==
   bool operator==( const Coordinate& c ) const
   {
     return( x == c.x &&
             y == c.y );
+  }
+
+  // Operator overload for =
+  Coordinate& operator=( const Coordinate& c )
+  {
+    x = c.x;
+    y = c.y;
+    return *this;
   }
 };
