3cfb4c6684daa9cdc5b58426cbb00e5352f106f8
jleung51 <jleung513@gmail.com>
2016-01-19 13:58:50 -0800

    Laby testing: Adding tests for ConnectRooms().

--- test/test_laby.cpp
+++ test/test_laby.cpp
@@ -1,7 +1,7 @@
 /*
  *
  * Author: Jeffrey Leung
- * Last edited: 2015-09-19
+ * Last edited: 2016-01-15
  *
  * This C++ file tests the Labyrinth class implementation.
  *
@@ -96,13 +96,128 @@ int main()
   std::cout << std::endl;
 
 
+  
+  std::cout << "________________________________________________"
+            << std::endl << std::endl
+            << "TESTING CONNECTROOMS():"
+            << std::endl << std::endl;
+
+  Coordinate c_0_0(0, 0);
+  Coordinate c_0_1(0, 1);
+  Coordinate c_1_0(1, 0);
+  Coordinate c_1_1(1, 1);
+  Coordinate c_2_1(2, 1);
+
+  std::cout << "Connecting horizontally adjacent rooms (0, 0) and (1, 0):"
+            << std::endl;
+  try
+  {
+    l1.ConnectRooms( c_0_0, c_1_0 );
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what();
+  }
+  std::cout << "Done." << std::endl << std::endl;
+
+  std::cout << "Connecting horizontally adjacent rooms (1, 1) and (0, 1):"
+            << std::endl;
+  try
+  {
+    l1.ConnectRooms( c_1_1, c_0_1 );
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what();
+  }
+  std::cout << "Done." << std::endl << std::endl;
+
+  std::cout << "Connecting vertically adjacent rooms (0, 0) and (0, 1):"
+            << std::endl;
+  try
+  {
+    l1.ConnectRooms(c_0_0, c_0_1);
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what();
+  }
+  std::cout << "Done." << std::endl << std::endl;
+
+  std::cout << "Connecting vertically adjacent rooms (1, 1) and (1, 0):"
+            << std::endl;
+  try
+  {
+    l1.ConnectRooms(c_1_1, c_1_0);
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what();
+  }
+  std::cout << "Done." << std::endl << std::endl;
+
+
+
+  std::cout << "Connecting diagonally adjacent rooms (0, 0) and (1, 1) "
+            << "(An error should be thrown):"
+            << std::endl;
+  try
+  {
+    l1.ConnectRooms(c_0_0, c_1_1);
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what();
+  }
+  std::cout << "Done." << std::endl << std::endl;
+
+  std::cout << "Connecting diagonally adjacent rooms (0, 1) and (1, 0) "
+            << "(An error should be thrown):"
+            << std::endl;
+  try
+  {
+    l1.ConnectRooms(c_0_1, c_1_0);
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what();
+  }
+  std::cout << "Done." << std::endl << std::endl;
+
+  std::cout << "Connecting diagonally adjacent rooms (0, 1) and (1, 0) "
+    << "(An error should be thrown):"
+    << std::endl;
+  try
+  {
+    l1.ConnectRooms(c_0_1, c_1_0);
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what();
+  }
+  std::cout << "Done." << std::endl << std::endl;
+
+  std::cout << "Connecting non-adjacent rooms (0, 0) and (2, 1) "
+            << "(An error should be thrown):"
+            << std::endl;
+  try
+  {
+    l1.ConnectRooms(c_0_0, c_2_1);
+  }
+  catch (const std::exception& e)
+  { 
+    std::cout << e.what();
+  }
+  std::cout << "Done." << std::endl << std::endl;
+
+
 
   std::cout << "________________________________________________" << std::endl;
   std::cout << std::endl;
   std::cout << "All tests completed." << std::endl;
   std::cout << std::endl;
   std::cout << "Press enter to exit.";
-  getchar();
+  getchar();getchar();
   std::cout << std::endl;
 
   return 0;
