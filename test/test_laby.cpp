f1a9ef959710f5c08e23043e93c44dd1b4874441
jleung51 <jleung513@gmail.com>
2016-02-04 14:57:11 -0800

    Laby testing: Formatting tests.

--- test/test_laby.cpp
+++ test/test_laby.cpp
@@ -31,70 +31,54 @@ void TestLabyrinthConstructor()
             << "  y size = 5" << std::endl;
 
   Labyrinth l1( 3, 5 );
-  std::cout << "Completed." << std::endl;
-  std::cout << std::endl;
+  std::cout << "Completed."
+            << std::endl
+            << std::endl;
 
-  std::cout << "Type 1 to attempt to create a Labyrinth with x size = 0."
-            << std::endl
-            << "Type 2 to attempt to create a Labyrinth with y size = 0."
-            << std::endl
-            << "Type 3 to attempt to create a Labyrinth with x size = "
-            << "y size = 0."
-            << std::endl
-            << "Type 0 to continue."
-            << std::endl
-            << "> ";
 
-  char c = ' ';
-  while ( c < '0' || c > '3' )
+
+  std::cout << "Create a Labyrinth with x size = 0: "
+            << "(An error should be thrown)"
+            << std::endl;
+  try
   {
-    std::cin >> c;
-    switch( c )
-    {
-      case( '1' ):
-        try
-        {
-          Labyrinth l_empty( 0, 10 );
-        }
-        catch( std::exception& e )
-        {
-          std::cout << e.what() << std::endl;
-        }
-        break;
-
-      case( '2' ):
-        try
-        {
-          Labyrinth l_empty( 10, 0 );
-        }
-        catch( std::exception& e )
-        {
-          std::cout << e.what() << std::endl;
-        }
-        break;
-
-      case( '3' ):
-        try
-        {
-          Labyrinth l_empty( 0, 0 );
-        }
-        catch( std::exception& e )
-        {
-          std::cout << e.what() << std::endl;
-        }
-        break;
-
-      case( '0' ):
-        break;
-
-      default:
-        std::cout << std::endl
-                  << "Error: That was not a valid input; please try again: ";
-        break;
-    }
+    Labyrinth l_trycatch( 0, 10 );
   }
-  std::cout << std::endl;
-  return;
+  catch( std::exception& e )
+  {
+    std::cout << e.what();
+  }
+  std::cout << "Completed."
+            << std::endl << std::endl;
+
+  std::cout << "Create a Labyrinth with y size = 0: "
+            << "(An error should be thrown)"
+            << std::endl;
+  try
+  {
+    Labyrinth l_trycatch( 10, 0 );
+  }
+  catch( std::exception& e )
+  {
+    std::cout << e.what();
+  }
+  std::cout << "Completed."
+            << std::endl << std::endl;
+
+  std::cout << "Create a Labyrinth with x size = "
+            << "y size = 0. "
+            << "(An error should be thrown)"
+            << std::endl;
+  try
+  {
+    Labyrinth l_trycatch( 0, 0 );
+  }
+  catch( std::exception& e )
+  {
+    std::cout << e.what();
+  }
+  std::cout << "Completed."
+            << std::endl << std::endl;
 }
 
 // This local function runs tests for the Labyrinth method ConnectRooms().
@@ -229,8 +213,8 @@ void TestLabyrinthConnectRooms()
 int main()
 {
   std::cout << std::endl
-            << "TESTING LABYRINTH.CPP IMPLEMENTATION" << std::endl
-            << "________________________________________________" << std::endl
+            << "TESTING LABYRINTH.CPP IMPLEMENTATION"
+            << std::endl
             << std::endl;
 
   TestLabyrinthConstructor();
@@ -241,7 +225,6 @@ int main()
   std::cout << "All tests completed." << std::endl;
   std::cout << std::endl;
   std::cout << "Press enter to exit.";
-  getchar();
   getchar();
   std::cout << std::endl;
 
